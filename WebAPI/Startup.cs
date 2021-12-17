89f7c606504278220b97298cf3a601e9087f2b4c
SenuberAgazade <senuberagazade996@gmail.com>
2021-12-17 17:54:39 +0400

    modified web api start , added cors

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -46,7 +46,7 @@ namespace WebAPI
 
             services.AddCors(options =>
             {
-                options.AddPolicy("AllowOrigin", builder => builder.WithOrigins("http://localhost:3000"));
+                options.AddPolicy("AllowOrigin", builder => builder.WithOrigins("http://localhost:4200"));
             });
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
@@ -78,7 +78,7 @@ namespace WebAPI
             }
 
 
-            app.UseCors(builder => builder.WithOrigins("http://localhost:3000").AllowAnyHeader());
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
